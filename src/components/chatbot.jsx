f9cee0a226ab88347334fa90a328aaccb2f02aa6
FkySaharat <saharat_d@cmu.ac.th>
2019-10-20 14:08:20 +0700

    EntertextInput-NewScrollbra

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -3,29 +3,30 @@ import React, {Component } from "react";
 import Listmessages from './messages';
 import Api from '../api.json';
 //import { storiesOf } from "@storybook/react";
-import ScrollToBottom from 'react-scroll-to-bottom';
 //import { Search } from "grommet-icons";
-import { Box, Image, Grommet, Text, TextInput, Button ,Grid,InfiniteScroll} from "grommet";
+import { Box, Grommet, TextInput, Button } from "grommet";
 //import { grommet } from "grommet/themes";
 //import { deepMerge } from "grommet/utils";
-import { css } from 'glamor';
-import { Close, Send, User } from "grommet-icons";
+import { Send } from "grommet-icons";
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
 
 class Chatbot extends Component {
     state={
       count:0,
-      messagebuffer:[{mode:null,time:null,type:null,payload:null}],
+      messagebuffer:[{mode:'',time:0,type:'message',payload:''}],
+     
     };
 
     constructor() {
         super();
-
+       
         this.handleaddmessage=this.handleaddmessage.bind(this);  
         this.handlemessage=this.handlemessage.bind(this);     
     }
 
-    
-
+  
     handleaddmessage(event){
      this.state.messagebuffer.push(event[0]);
      this.setState({some:event[0],messagebuffer:this.state.messagebuffer});
@@ -35,7 +36,7 @@ class Chatbot extends Component {
       var attr=[{mode:'client',time:0,type:'message',payload:para}];
       this.handleaddmessage(attr);
     
-      fetch(Api.Url+para+'&sessionId=1',{
+      fetch(Api.Url+para+'&sessionId=2',{
             method:'GET',
             headers: new Headers({
                     'Content-Type': 'application/json',
@@ -55,42 +56,71 @@ class Chatbot extends Component {
                 this.handleaddmessage(attr);
               }
             }
-            //if(data.result.fulfillment.messages["imageUrl"]){
-            
-            //  this.handleaddmessage(data.result.fulfillment.messages("imageUrl"));
-            //}
+            this.scrollToWithContainer()
             console.log(data.result.fulfillment.messages[0].speech);
         }).catch((error) => console.log(error))
     }
   
-    
-    
+   
+    scrollToWithContainer() {
 
-    render() {
-      const ROOT_CSS = css({
-        height: 600,
-        width:  360
+      let goToContainer = new Promise((resolve, reject) => {
+  
+        Events.scrollEvent.register('end', () => {
+          resolve();
+          Events.scrollEvent.remove('end');
+        });
+  
+        scroller.scrollTo('scroll-container', {
+          duration: 8,
+          delay: 0,
+          smooth: 'easeInOutQuart'
+        });
+  
       });
+  
+      goToContainer.then(() =>
+        scroller.scrollTo('test1', {
+          duration: 100,
+          delay: 0,
+          smooth: 'easeInOutQuart',
+          containerId: 'scroll-container'
+        }));
+    }
+
+    handleKeyPress = (event) => {
+      if(event.key === 'Enter'){
+        this.handlemessage(this.refs.messages.value)
+      }
+    }
+    render() {
+    
       
+      
+      console.log("lenght",this.state.messagebuffer[this.state.messagebuffer.length-1]);
       return (
         <Grommet full={true}  > 
-        <Box align="center"  background="dark-1"  height="full">
-            <Box align="center" background="linear-gradient(102.77deg, #865ED6 -9.18%, #18BAB9 209.09%)" height="80%" width="medium" overflow="auto" round={{ "corner": "bottom"}}>
-              <ScrollToBottom className={ROOT_CSS}>
-              {this.state.messagebuffer.map(m=><Listmessages value={m} />)}
-              </ScrollToBottom>
+        <Box align="center"  background="dark-1" height="full" >
+            <Box  background="linear-gradient(102.77deg, #865ED6 -9.18%, #18BAB9 209.09%)"  overflow={{"vertical": "scroll", "horizontal": "hidden"}} height="80%" width="medium"   round={{ "corner": "bottom"}} id="scroll-container">
+              
+            {this.state.messagebuffer.map(m=>{
+                if(m!==this.state.messagebuffer[this.state.messagebuffer.length-1]){
+                  return <Listmessages value={m} name="test1"  />
+                }
+                else{
+                  return <Listmessages value={m} />
+                }
+            })}
+            
             </Box>
-              
+            
             <Box margin={{"top":"xsmall"}} width="medium" align="center" direction="row" justify="between">
-              <TextInput  type="text" ref="messages" />
-              <Button icon={<Send />} margin="xsmall" pad="small" primary  onClick={()=>this.handlemessage(this.refs.messages.value)} />  
-            </Box> 
-              
-           
+              <TextInput  type="text" placeholder="Type here" ref="messages" onKeyPress={this.handleKeyPress}/>
+              <Button icon={<Send />} margin="xsmall" pad="small" primary className="test1" to="test1" onClick={()=>this.handlemessage(this.refs.messages.value)} />  
+            </Box>           
+            
         </Box>
-            
-            
-         
+       
         </Grommet>
       );
     }
